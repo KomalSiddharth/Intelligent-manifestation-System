990fdc0efca354273d18fbe5c36322e79c40e23d
komal siddharth <komalsiddharth814@gmail.com>
2026-05-15 15:20:26 +0530

    Update build with UUID fix

--- app-81mqyjlan9xd/dist/assets/textarea-UXI83LuG.js
+++
@@ -1,6 +0,0 @@
-import{c as r,j as i,d as t}from"./index-ycyTXq2x.js";/**
- * @license lucide-react v0.553.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const s=[["path",{d:"M22 17a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 21.286V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2z",key:"18887p"}]],n=r("message-square",s);function d({className:e,...a}){return i.jsx("textarea",{"data-slot":"textarea",className:t("border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",e),...a})}export{n as M,d as T};
